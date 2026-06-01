184fe957ffa7e69bbc0376520b295d1ce5eecf97
arpitshakya10 <shakya.arpit10@gmail.com>
2026-06-01 22:20:06 +0530

    Create 1_Two_sum.cpp

---
+++ Week_1/Day_1/1_Two_sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            int first=nums[i];
+            int sec=target-first;
+
+            if(m.find(sec)!=m.end()){
+                return {i,m[sec]};
+            }
+            m[first]=i;
+        }
+        return {};
+    }
+};
