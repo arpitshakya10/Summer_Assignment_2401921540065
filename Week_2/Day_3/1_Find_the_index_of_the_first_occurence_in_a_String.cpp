8651f00300703f133758b99b4ece7143702aece4
arpitshakya10 <shakya.arpit10@gmail.com>
2026-06-10 20:28:03 +0530

    Create 1_Find_the_index_of_the_first_occurence_in_a_String.cpp

---
+++ Week_2/Day_3/1_Find_the_index_of_the_first_occurence_in_a_String.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int h=haystack.size(),n=needle.size(),j;
+        for(int i=0;i<=h-n;i++){
+            for( j=0;j<n;j++){
+                if(haystack[i+j]!=needle[j]){
+                    break;
+                }
+            }
+            if(j==n){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
