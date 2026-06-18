c81855ab2a9f90c30638cf1581a1adedcdb14ff0
arpitshakya10 <shakya.arpit10@gmail.com>
2026-06-19 01:09:30 +0530

    Create 1_Daily_Temperature.cpp

---
+++ Week_3/Day_4/1_Daily_Temperature.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        int n = temp.size();
+        vector<int> ans(n, 0);
+        stack<int> st;
+        
+        for(int i = 0; i < n; i++) {
+            while(!st.empty() && temp[i] > temp[st.top()]) {
+                int prevDay = st.top();
+                st.pop();
+                ans[prevDay] = i - prevDay;
+            }
+            st.push(i);
+        }
+        
+        return ans;
+    }
+};
