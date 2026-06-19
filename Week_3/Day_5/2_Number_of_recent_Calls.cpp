8d51087bc89117d2a94fcc3fe72aa4df4babd716
arpitshakya10 <shakya.arpit10@gmail.com>
2026-06-19 23:59:09 +0530

    Update 2_Number_of_recent_Calls.cpp

--- Week_3/Day_5/2_Number_of_recent_Calls.cpp
+++ Week_3/Day_5/2_Number_of_recent_Calls.cpp
@@ -1,1 +1,12 @@
-
+class RecentCounter {
+public:
+    queue<int> calls;
+    RecentCounter() {}
+    int ping(int t) {
+        calls.push(t);
+        while (!calls.empty() && calls.front() < t - 3000) {
+            calls.pop();
+        }
+        return calls.size();
+    }
+};
