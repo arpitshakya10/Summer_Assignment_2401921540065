487759c650568a3a32f6c33ee14cfef07dbca666
arpitshakya10 <shakya.arpit10@gmail.com>
2026-06-19 23:58:45 +0530

    Update 1_Implement_Queue_using_Stacks.cpp

--- Week_3/Day_5/1_Implement_Queue_using_Stacks.cpp
+++ Week_3/Day_5/1_Implement_Queue_using_Stacks.cpp
@@ -1,1 +1,32 @@
-
+class MyQueue {
+public:
+    stack<int> input;
+    stack<int> output;
+    MyQueue() {}
+    void push(int x) {
+        input.push(x);
+    }
+    int pop() {
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        int frontElement = output.top();
+        output.pop();
+        return frontElement;
+    }
+    int peek() {
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        return output.top();
+    }
+    bool empty() {
+        return input.empty() && output.empty();
+    }
+};
