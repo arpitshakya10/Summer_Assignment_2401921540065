0f32ff9edeb515ee1fd55907726ae5f0236fbe09
arpitshakya10 <shakya.arpit10@gmail.com>
2026-06-28 17:08:49 +0530

    Update 3_Serialize_and_Deserialize_Binary_Tree.cpp

--- Week_4/Day_5/3_Serialize_and_Deserialize_Binary_Tree.cpp
+++ Week_4/Day_5/3_Serialize_and_Deserialize_Binary_Tree.cpp
@@ -1,1 +1,37 @@
+class Codec {
+public:
 
+    string serialize(TreeNode* root) {
+        if(root==NULL){
+            return "N,";
+        }
+
+        string ans = to_string(root->val) + ",";
+
+        ans += serialize(root->left);
+        ans += serialize(root->right);
+
+        return ans;
+    }
+
+    TreeNode* helper(stringstream& ss){
+        string str;
+        getline(ss, str, ',');
+
+        if(str=="N"){
+            return NULL;
+        }
+
+        TreeNode* root = new TreeNode(stoi(str));
+
+        root->left = helper(ss);
+        root->right = helper(ss);
+
+        return root;
+    }
+
+    TreeNode* deserialize(string data) {
+        stringstream ss(data);
+        return helper(ss);
+    }
+};
