8e9d584a9aa16f53a2456f6bec39c4b8ce5b627e
arpitshakya10 <shakya.arpit10@gmail.com>
2026-06-28 17:08:14 +0530

    Update 2_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp

--- Week_4/Day_5/2_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
+++ Week_4/Day_5/2_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
@@ -1,1 +1,32 @@
+class Solution {
+public:
+    int index = 0;
 
+    TreeNode* build(vector<int>& preorder, int start, int end,
+                    unordered_map<int,int>& mp){
+
+        if(start > end){
+            return NULL;
+        }
+
+        int curr = preorder[index++];
+        TreeNode* root = new TreeNode(curr);
+
+        int pos = mp[curr];
+
+        root->left = build(preorder, start, pos-1, mp);
+        root->right = build(preorder, pos+1, end, mp);
+
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        unordered_map<int,int> mp;
+
+        for(int i=0; i<inorder.size(); i++){
+            mp[inorder[i]] = i;
+        }
+
+        return build(preorder, 0, inorder.size()-1, mp);
+    }
+};
